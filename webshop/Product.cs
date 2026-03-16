aeec823f0ce1876fb8ea6f2e8a815d6f2a1e2345
seppe-ui <rawoensseppe@gmail.com>
2026-03-16 10:49:06 +0100

    rest

--- webshop/Product.cs
+++ webshop/Product.cs
@@ -8,11 +8,13 @@ namespace webshop
 {
     internal class Product
     {
+        List<string> Product = new List<string>();
+
         public int Id { get; set; }
         public int Naam { get; set; }
         public int Prijs { get; set; }
         public int Voorraad { get; set; }
-        List<string> Product = new List<string>();
+        
 
 
     }
